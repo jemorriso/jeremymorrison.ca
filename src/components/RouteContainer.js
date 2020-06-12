514c8b02e2e60428a5c382f63cd5de90daf7d713
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-06-11 21:09:40 -0700

    Fix fade issues

--- src/components/RouteContainer.js
+++ src/components/RouteContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
@@ -9,35 +10,71 @@ import Contact from './Contact';
 import Landing from './Landing';
 import PortfolioItemDetail from './PortfolioItemDetail';
 
+const Wrapper = styled.div`
+  .fade-enter {
+    opacity: 0.01;
+  }
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 300ms ease-in;
+  }
+  .fade-exit {
+    opacity: 1;
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0.01;
+    transition: opacity 300ms ease-in;
+  }
+
+  div.transition-group {
+    position: relative;
+  }
+  section.route-section {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+`;
+
 function RouteContainer(props) {
   const { location } = props;
 
   return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} classNames="fade" timeout={1000}>
-        <Switch location={location}>
-          <Route path="/about">
-            <About />
-          </Route>
-          {/* TODO: clean up routing order */}
-          <Route path="/portfolio/:portfolioDetail">
-            <PortfolioItemDetail />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Landing />
-          </Route>
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <Wrapper>
+      <TransitionGroup className="transition-group">
+        <CSSTransition
+          key={location.key}
+          classNames="fade"
+          timeout={{ enter: 1000, exit: 1000 }}
+        >
+          <section className="route-section">
+            <Switch location={location}>
+              <Route path="/about">
+                <About />
+              </Route>
+              {/* TODO: clean up routing order */}
+              <Route path="/portfolio/:portfolioDetail">
+                <PortfolioItemDetail />
+              </Route>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/resume">
+                <Resume />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </section>
+        </CSSTransition>
+      </TransitionGroup>
+    </Wrapper>
   );
 }
 
