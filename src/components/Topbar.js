f1ebba4d15cd9f2def7614b7dafea6e8479614c0
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-06-16 18:06:31 -0700

    Add mobile topbar component

---
+++ src/components/Topbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import Brand from './Brand';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+  top: 0;
+  width: 100%;
+  background-color: lightgoldenrodyellow;
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: 850px) {
+    display: none;
+  }
+`;
+
+const Icon = styled.div`
+  min-height: 1em;
+  min-width: 1em;
+  background-color: #666;
+`;
+
+function Topbar(props) {
+  const { toggleSidebar } = props;
+  return (
+    <Wrapper>
+      <Icon onClick={toggleSidebar} />
+      <Brand />
+    </Wrapper>
+  );
+}
+
+Topbar.propTypes = {
+  toggleSidebar: PropTypes.func.isRequired,
+};
+export default Topbar;
