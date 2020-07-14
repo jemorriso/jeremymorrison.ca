1d4b2bd84cc9d95fb0cc82a018dde84e9767b9f9
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-07-14 15:15:01 -0700

    Change icon font size. Height 100% for more flexible content placement.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,24 @@ import PropTypes from 'prop-types';
 import SectionHeader from './SectionHeader';
 import Socials from './Socials';
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  height: 100%;
+`;
+
+const FlexWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: flex-start; */
+  justify-content: flex-end;
+`;
 
 function Contact(props) {
   const { socials } = props;
   return (
     <Wrapper>
       <SectionHeader title="Contact" />
-      <Socials socials={socials} fontSize="4.8rem" />
+      <Socials socials={socials} fontSize="11rem" />
     </Wrapper>
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import SectionHeader from './SectionHeader';
 import Socials from './Socials';
+
+const Wrapper = styled.section`
+  height: 100%;
+`;
 
 function Resume(props) {
   const resumeSocials = [
@@ -12,10 +15,10 @@ function Resume(props) {
     },
   ];
   return (
-    <section>
+    <Wrapper>
       <SectionHeader title="Resume" />
-      <Socials socials={resumeSocials} fontSize="4.8rem" />
-    </section>
+      <Socials socials={resumeSocials} fontSize="11rem" />
+    </Wrapper>
   );
 }
 
