bce35de6e0019e6373c3a3e34c3d0f10aadb9592
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-10 14:04:15 -0700

    Add babel config for Styled Components
    Necessary configuration for Next.js requires adding _document.js file.
    See styled-components docs.

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+// https://www.smashingmagazine.com/2020/09/comparison-styling-methods-next-js
+// https://github.com/vercel/next.js/tree/master/examples/with-styled-components
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
