646d21ece453c1d4881f81bbed04868b1f40b0cc
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-09 17:45:46 -0700

    Add social media and nav links

---
+++ src/config.js
@@ -0,0 +1,33 @@
+export const email = 'jwill.morrison@gmail.com';
+export const socialMedia = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/JeMorriso',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/_jeremymorrison/',
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/jwill-morrison/',
+  },
+];
+export const navLinks = [
+  {
+    name: 'About',
+    url: '/#about',
+  },
+  {
+    name: 'Experience',
+    url: '/#jobs',
+  },
+  {
+    name: 'Projects',
+    url: '/#projects',
+  },
+  {
+    name: 'Contact',
+    url: '/#contact',
+  },
+];
