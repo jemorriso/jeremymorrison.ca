8077a61fa2296c544c981560ffcc6dcc033ae9c1
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-10 22:59:47 -0700

    Add breakpoints, fonts, mixins

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,6 +1,38 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --mobileS: 330px;
+    --mobileM: 400px;
+    --mobileL: 480px;
+    --tabletS: 600px;
+    --tabletL: 768px;
+    --desktopXS: 900px;
+    --desktopS: 1080px;
+    --desktopM: 1200px;
+    --desktopL: 1400px;
+
+    --font-size-xxs: 12px;
+    --font-size-xs: 13px;
+    --font-size-sm: 14px;
+    --font-size-md: 16px;
+    --font-size-lg: 18px;
+    --font-size-xl: 20px;
+    --font-size-xxl: 22px;
+    --font-size-heading: 32px; 
+  }
+    
+  html {
+    box-sizing: border-box;
+  }
+
+  // See 'Universal Box Sizing with inheritance': https://css-tricks.com/box-sizing/  
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
@@ -9,4 +41,13 @@ export const GlobalStyle = createGlobalStyle`
     font-family: ${({ theme }) => theme.fontSans};
     // font-family: ${({ theme }) => theme.fontMono};
   }
+
+  a {
+    /* display: inline-block; */
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    color: inherit;
+  }
 `;
+
+export default GlobalStyle;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,3 +1,5 @@
+import { css } from 'styled-components';
+
 const nord = {
   body: '#1D2129',
   text: '#D1DBE5',
@@ -9,4 +11,18 @@ const fonts = {
   fontMono: 'Courier, Monaco, monospace',
 };
 
-export const theme = { ...nord, ...fonts };
+const mixins = {
+  flexCenter: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  flexBetween: css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+};
+
+export const theme = { ...nord, ...fonts, mixins };
