8544790ba86919d78486c6fe955318b32b3a8d84
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-10 17:20:58 -0700

    Add custom alias and index for components
    jsconfig.json is the Next.js way to add module path aliases. See
    https://nextjs.org/docs/advanced-features/module-path-aliases

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// https://stackoverflow.com/questions/44092341/how-do-index-js-files-work-in-react-component-directories
+// https://stackoverflow.com/questions/39999282/re-export-default-in-es-6-modules
+export { default as Nav } from './nav';
