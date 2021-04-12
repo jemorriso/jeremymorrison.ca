bce4063363daa463608fa9f3facf0c6dfcf63408
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-11 17:42:03 -0700

    Add mdx, jest

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+const emoji = require('remark-emoji');
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/,
+  options: {
+    remarkPlugins: [emoji],
+  },
+});
+
+module.exports = withMDX({
   future: {
     webpack5: true,
   },
-};
+});
