8ee1bf867f1f50792f21513c328898e2f74158eb
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-14 12:29:11 -0700

    Update next config + project config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
-const emoji = require('remark-emoji');
+module.exports = {
+  // Cannot use webpack5 with the config that fixes the module error.
+  // future: {
+  //   webpack5: true,
+  // },
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/,
-  options: {
-    remarkPlugins: [emoji],
+  // I was getting `can't resolve 'fs'` when I imported 2 functions from mdx.js
+  // and tried to use them both in getStaticProps. See the link, for some reason
+  // it can't figure out that I'm using them only on the server side. I think
+  // it's because one calls the other and it gets confused.
+  // https://javascript.plainenglish.io/understand-code-splitting-in-next-js-by-solving-a-common-error-db61a9b1b1a1
+  webpack: (config, { isServer }) => {
+    // https://github.com/vercel/next.js/issues/7755
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      };
+    }
+    return config;
   },
-});
-
-module.exports = withMDX({
-  future: {
-    webpack5: true,
-  },
-});
+};
