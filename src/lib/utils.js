a93edf362ce16250de6c651a8955554f611e29c4
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-15 12:10:24 -0700

    Add sort by rank and date function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,3 +2,24 @@ export const zip = (a, b) => {
   const c = a.map((el, i) => [el, b[i]]);
   return c;
 };
+
+const rankCompare = (a, b) => {
+  if (!(a.rank === undefined) && !(b.rank === undefined)) {
+    return a.rank - b.rank;
+  }
+  if (!(a.rank === undefined)) {
+    return -1;
+  }
+  if (!(b.rank === undefined)) {
+    return 1;
+  }
+  return 0;
+};
+
+export const sortRankDate = (arr) => {
+  return arr.sort(
+    ({ frontMatter: a }, { frontMatter: b }) =>
+      rankCompare(a, b) || b.date.localeCompare(a.date)
+    // a.rank - b.rank || b.date.localeCompare(a.date)
+  );
+};
