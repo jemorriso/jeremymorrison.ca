7a952abd1d84556c5d83212d95adeff09cf58963
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-04-15 12:11:09 -0700

    Add more components to index

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,11 @@
 // https://stackoverflow.com/questions/39999282/re-export-default-in-es-6-modules
 export { default as Nav } from './nav';
 export { default as Menu } from './menu';
+export { default as Layout } from './layout';
+export { default as Project } from './project';
+export { default as Section } from './section';
+export { default as Hero } from './sections/hero';
+export { default as About } from './sections/about';
+export { default as Jobs } from './sections/jobs';
+export { default as Projects } from './sections/projects';
+export { default as Contact } from './sections/contact';
